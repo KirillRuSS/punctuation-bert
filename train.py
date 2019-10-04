b762b5a904a16e2d3ed6abb0f67f06c199656a45
kirillribkin1998@gmail.com <Kepler452b>
2019-10-04 20:07:20 +0300

    Добавлено обучение модели

--- train.py
+++ train.py
@@ -3,3 +3,15 @@ from model import Model
 
 data = Dataset()
 model = Model()
+
+seg_input = np.zeros((len(data.validation_outputs), 512))
+
+model.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(
+    [data.train_inputs, data.train_masks, seg_input],
+    data.train_outputs,
+    validation_data=([data.validation_inputs, data.validation_masks, seg_input], data.validation_outputs),
+    epochs=1,
+    batch_size=32
+)
